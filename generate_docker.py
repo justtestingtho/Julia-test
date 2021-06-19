d9424700d374a78ba2b76dbe3cc68a1bf2f69353
justtestingtho <anasabdelrehim@gmail.com>
2021-06-19 18:41:44 -0400

    update json to test + docker_gen script

--- generate_docker.py
+++ generate_docker.py
@@ -4,53 +4,40 @@ import re
 
 def main():
     
-    json_path = "ex.json"
-    docker_path = "Dockerfile"
+    json_path = "training_config.json"
+    file_path = "Dockerfile"
+
     
     try:
         j_file = open(json_path, "r")
-        d_file = open(docker_path, "r")
-
         l_json = json.load(j_file)
-        l_docker = d_file.readlines()
-
         package_list = l_json.get("Packages")
 
         pkg_commands = []
-        counter = 0
+        l_size = len(package_list)
+        f = open("Dockerfile", 'w')
+        f.write("FROM julia:latest")
+        f.write("\n")
+        f.write("\n")
+        f.write("RUN julia -e 'import Pkg; Pkg.update()' && \\")
+        f.write("\n")
         for pkg in package_list:
-            if counter != len(package_list) - 1:
+            if pkg != package_list[-1]:
                 cmd = f'    julia -e \'using Pkg; pkg"add {pkg}"; pkg"precompile"\' && \\\n'
+                f.write(cmd)
 
             else:
                 cmd = f'    julia -e \'using Pkg; pkg"add {pkg}"; pkg"precompile"\' \n'
-            
-            pkg_commands.append(cmd)
-            counter += 1
- 
-        remove_pkgs = []
+                f.write(cmd)
 
-        existing_pkg_index = [i for i, s in enumerate(l_docker, start=0) if re.search('.*using Pkg', s)]
-
-        for index, lines in enumerate(l_docker):
-
-            if index not in existing_pkg_index:
-                remove_pkgs.append(lines)
-
-        RUN_index = [i for i, s in enumerate(l_docker, start=0) if re.search('.*RUN', s)]
-
-        insert_at = RUN_index[0] + 1
-
-        remove_pkgs[insert_at:insert_at] = pkg_commands
-
-        store_updated = open("Dockerfile", "w")
-
-        for update in remove_pkgs:
-            store_updated.write(update)
-    
-        store_updated.close()
-        
-        print("Updated Docker File is stored. Please check...")
+        f.write("\n")            
+        f.write("ENV PATH='/opt/program:${PATH}'")
+        f.write("\n")
+        f.write("COPY . /opt/program")
+        f.write("\n")
+        f.write("ENTRYPOINT [ 'julia', './algo/train' ]")
+        f.close()
+        print("Updated Docker File is stored")
     
     except Exception as e:
         print("Exception :", e)
